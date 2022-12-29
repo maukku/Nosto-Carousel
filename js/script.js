a927f3d635292b5589c507d625e4780e2bc1ec14
maukku <maurorisso17@gmail.com>
2022-12-29 18:19:57 +0100

    mock carousel added

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $("#carousel").slick({
+      slidesToShow: 3,
+      dots: true,
+      
+      infinite: false,
+      centerPadding: 10  ,
+      variableWidth: false  
+      
+    });
+  });
+  
