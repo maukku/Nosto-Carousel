59ca337a9dc84e9ec01d3b8bc0723bc9a3126513
maukku <maurorisso17@gmail.com>
2022-12-30 14:15:08 +0100

    basic functionality added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,84 @@
-$(document).ready(function () {
-    $("#carousel").slick({
-      slidesToShow: 3,
-      dots: true,
-      
-      infinite: false,
-      centerPadding: 10  ,
-      variableWidth: false  
-      
+$.ajax({
+  url: "https://api.nosto.com/v1/graphql",
+  type: "POST",
+  contentType: "application/graphql",
+  headers: {
+    Authorization:
+      "Basic " +
+      btoa(
+        //sadly no way to hide it without using a node js app
+        ":" + "N7QnHtiseaaAtartB16sQ7jUcNAm0HgsTxTnwTX08GQ85EYShd90zN3qiYiDjVsq"
+      ),
+  },
+  data: `query {
+    products (limit: 50) {
+      products {
+        name
+        price
+        listPrice
+        brand
+        imageUrl
+        alternateImageUrls
+        url
+        scores {
+          week {
+            views
+            buys
+          }
+        }
+      }
+    }
+  }`,
+  success: function (response) {
+    let max = 0;
+
+    let mostViewedProduct = null;
+    let carousel = $("#carousel");
+    response.data.products.products.forEach((product) => {
+      if (product.scores.week.views > max) {
+        mostViewedProduct = product;
+        max = product.scores.week.views;
+      }
+      console.log(product.scores.week.views);
+
+      let item = $("<div  class='item'>");
+      let formattedPrice = parseFloat(product.price); // Parse the price as a float
+
+      item.html(
+        '<img src="' +
+          product.imageUrl +
+          '" class="product-img" data-url="' +
+          product.url +
+          '"onerror="$(this).parent().remove()" ><div class="product-name">' +
+          product.name +
+          "</div><div class='product-brand'>" +
+          product.brand +
+          "</div>" +
+          "</div><div class='product-price'>€" +
+          formattedPrice +
+          "</div>"
+      );
+      carousel.append(item);
     });
-  });
-  
+
+    $("#bestseller-item").html(
+      '<img src="' + mostViewedProduct.imageUrl + '">'
+    );
+    $(".product-img").click((event) => {
+      let productURL = $(event.target).data("url");
+      window.location.assign(productURL);
+    });
+
+    $(document).ready(() => {
+      $("#carousel").slick({
+        slidesToShow: 3,
+        dots: false,
+        draggable: false,
+
+        infinite: false,
+        centerPadding: 10,
+        variableWidth: true,
+      });
+    });
+  },
+});
