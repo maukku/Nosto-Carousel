24e76074f5098b54febcb70157c93d7ca79119aa
maukku <maurorisso17@gmail.com>
2022-12-31 13:37:53 +0100

    Convert JQuery code to equivalent ES6 syntax

--- js/script.js
+++ js/script.js
@@ -1,115 +1,126 @@
-$.ajax({
-  url: "https://api.nosto.com/v1/graphql",
-  type: "POST",
-  contentType: "application/graphql",
-  headers: {
-    Authorization:
-      "Basic " +
-      btoa(
-        //sadly no way to hide it without using a node js app
-        ":" + "N7QnHtiseaaAtartB16sQ7jUcNAm0HgsTxTnwTX08GQ85EYShd90zN3qiYiDjVsq"
-      ),
-  },
-  data: `query {
-    products (limit: 50) {
-      products {
-        name
-        price
-        listPrice
-        brand
-        imageUrl
-        alternateImageUrls
-        url
-        scores {
-          week {
-            views
-            buys
+// First we define an async function to handle the AJAX request
+const getProducts = async () => {
+  // Define the GraphQL query as a template literal
+  const query = `
+    query {
+      products (limit: 50) {
+        products {
+          name
+          price
+          listPrice
+          brand
+          imageUrl
+          alternateImageUrls
+          url
+          scores {
+            week {
+              views
+              buys
+            }
           }
         }
       }
     }
-  }`,
-  success: (response) => {
-    let max = 0;
+  `;
 
-    let mostBoughtProduct = null;
-    let carousel = $("#carousel");
-    response.data.products.products.forEach((product) => {
-      if (product.scores.week.views > max) {
-        mostBoughtProduct = product;
-        max = product.scores.week.views;
-      }
+  // Make the AJAX request using fetch and providing the necessary options
+  const response = await fetch("https://api.nosto.com/v1/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/graphql",
+      Authorization: `Basic ${btoa(
+        ":" + "N7QnHtiseaaAtartB16sQ7jUcNAm0HgsTxTnwTX08GQ85EYShd90zN3qiYiDjVsq"
+      )}`,
+    },
+    body: query,
+  });
 
-      let item = $("<div  class='item'>");
-      let formattedPrice = parseFloat(product.price); // Parse the price as a float
+  // Convert the response to JSON
+  const data = await response.json();
 
-      item.html(
-        '<img src="' +
-          product.imageUrl +
-          '" class="product-img" data-url="' +
-          product.url +
-          '" onerror="$(this).parent().remove()" data-alternateurl="' +
-          product.alternateImageUrls +
-          '">' +
-          '<div class="product-name">' +
-          product.name +
-          "</div>" +
-          '<div class="product-brand">' +
-          product.brand +
-          "</div>" +
-          '<div class="product-price">€' +
-          formattedPrice +
-          "</div>"
-      );
+  // Extract the products from the response data
+  const products = data.data.products.products;
 
-      if (product.name !== mostBoughtProduct.name) {
-        carousel.append(item);
+  let mostBoughtProduct = null;
+
+  // Loop through the products and find the one with the highest number of views
+  products.forEach((product) => {
+    if (product.scores.week.views > 0) {
+      mostBoughtProduct = product;
+    }
+  });
+
+  // Select the carousel element
+  const carousel = document.querySelector("#carousel");
+
+  // Loop through the products again, creating elements for each product
+  products.forEach((product) => {
+    // Parse the product price as a float
+    const formattedPrice = parseFloat(product.price);
+
+    // Create a new div element for the product
+    const item = document.createElement("div");
+    item.classList.add("item");
+
+    // Set the inner HTML for the product element
+    item.innerHTML = `
+      <img src="${product.imageUrl}" class="product-img" data-url="${product.url}" onerror="$(this).parent().remove()" data-alternateurl="${product.alternateImageUrls}">
+      <div class="product-name">${product.name}</div>
+      <div class="product-brand">${product.brand}</div>
+      <div class="product-price">€${formattedPrice}</div>
+    `;
+
+    // Only append the product element to the carousel if it is not the most bought
+    if (product.name !== mostBoughtProduct.name) {
+      carousel.appendChild(item);
+    }
+  });
+
+  // Add a hover event listener to the product images to switch between the main and alternate images
+  document.querySelectorAll(".product-img").forEach((img) => {
+    let originalImageUrl;
+    img.addEventListener("mouseenter", (event) => {
+      originalImageUrl = img.getAttribute("src");
+      const alternateImageUrls = img.getAttribute("data-alternateurl");
+      if (alternateImageUrls && alternateImageUrls.length > 0) {
+        img.setAttribute("src", alternateImageUrls);
       }
     });
-
-    let originalImageUrl;
-    $(".product-img").hover(
-      (event) => {
-        originalImageUrl = $(event.target).attr("src");
-
-        let alternateImageUrls = $(event.target).data("alternateurl");
-        if (alternateImageUrls && alternateImageUrls.length > 0) {
-          $(event.target).attr("src", alternateImageUrls);
-        }
-      },
-      (event) => {
-        if (originalImageUrl) {
-          $(event.target).attr("src", originalImageUrl);
-        }
+    img.addEventListener("mouseleave", (event) => {
+      if (originalImageUrl) {
+        img.setAttribute("src", originalImageUrl);
       }
-    );
+    });
+  });
 
-    $("#bestseller-item").html(
-      '<img class="product-img" src="' +
-        mostBoughtProduct.imageUrl +
-        '" data-url="' +
-        mostBoughtProduct.url +
-        "    >" +
-        '<div class="bestseller-label"></div>'
-    );
+  // Set the inner HTML for the most bought product element
+  document.querySelector("#bestseller-item").innerHTML = `
+    <img class="product-img" src="${mostBoughtProduct.imageUrl}" data-url="${mostBoughtProduct.url}">
+    <div class="bestseller-label">Best seller this week!</div>
+  `;
 
-    $(".product-img ").click((event) => {
-      let productURL = $(event.target).data("url");
+  // Add a click event listener to the product images to navigate to the product URL
+  document.querySelectorAll(".product-img").forEach((img) => {
+    img.addEventListener("click", (event) => {
+      const productURL = img.getAttribute("data-url");
       window.location.assign(productURL);
     });
+  });
 
-    $(document).ready(() => {
-      $("#carousel").slick({
-        slidesToShow: 4,
-        adaptiveHeight: false,
-
-        dots: false,
-        draggable: false,
-        infinite: false,
-        centerPadding: 0,
-        draggable: false,
-        variableWidth: true,
-      });
+  // Initialize the carousel using the Slick library
+  $(document).ready(() => {
+    $("#carousel").slick({
+      slidesToShow: 4,
+      adaptiveHeight: true,
+      dots: false,
+      draggable: false,
+      infinite: false,
+      centerPadding: 0,
+      draggable: false,
+      variableWidth: true,
     });
-  },
-});
+  });
+};
+
+// Call the getProducts function to make the initial AJAX request
+getProducts();
